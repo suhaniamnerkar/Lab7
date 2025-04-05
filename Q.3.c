61437e3d9dcb12b16860d13434c09e3526859037
Suhani Amnerkar <suhaniamnerkar@gmail.com>
2025-04-06 02:38:46 +0530

    Update Q.3.c

--- Q.3.c
+++ Q.3.c
@@ -1,90 +1,117 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node{
-    int a;
-    struct node*next;
-    struct node*prev;
+
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
 };
 
-struct node*create_node(int data){
-    struct node*cnode=(struct node*)malloc(sizeof(struct node*));
-    cnode->a=data;
-    cnode->next=NULL;
-    cnode->prev=NULL;
-    return cnode;
+
+struct Stack {
+    struct Node* top;
+};
+
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (!newNode) {
+        printf("Memory allocation error!\n");
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
 }
 
-void display(struct node*head){
-    while(head!=NULL){
-        printf("%d\n",head->a);
-        head=head->next;
-    }
+
+void initStack(struct Stack* stack) {
+    stack->top = NULL;
 }
 
-struct node*input(int data,struct node*head){
-    struct node*nnode=create_node(data);
-    if(head==NULL){
-        head=nnode;
-        return head;
-    }
-    struct node*temp=head;
-    while(temp->next!=NULL){
-        temp=temp->next;
-    }
-    temp->next=nnode;
-    nnode->prev=temp;
-    return head;
+
+int isEmpty(struct Stack* stack) {
+    return stack->top == NULL;
 }
 
-struct node*deletion(int m,int n, struct node*head){
-    
-    struct node*temp=head;
-    if(m==1){
-        head=temp->next;
-        temp->next=NULL;
-        free(temp);
-    }
-    else if(m>1 && m<n){
-        for(int i=1;i<m-1;i++){
-            temp=temp->next;
-        }
-        struct node*temp2=temp->next;
-        temp->next=temp2->next;
-        temp2->next->prev=temp;
-        free(temp2);
-        }
-        
-        else if(m==n){
-            struct node*prevnode;
-            while(temp->next!=NULL){
-                prevnode=temp;
-                temp=temp->next;
-            }
-            prevnode->next=NULL;
-            free(temp);
 
-        }
-    return head;
+void push(struct Stack* stack, int data) {
+    struct Node* newNode = createNode(data);
+    if (isEmpty(stack)) {
+        stack->top = newNode;
+    } else {
+        newNode->next = stack->top;
+        stack->top->prev = newNode;
+        stack->top = newNode;
+    }
+    printf("%d pushed onto the stack\n", data);
 }
 
-int main(){
-    int n;
-    struct node*nnode,*head;
-    head=NULL;
-    int data;
-    printf("Enter the number of elements you want to enter");
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&data);
-        head=input(data,head);
+
+int pop(struct Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Stack is empty! Cannot pop.\n");
+        return -1;
     }
-    int m;
-    printf("Enter the position at which you want to delete element:");
-    scanf("%d",&m);
 
-    head=deletion(m,n,head);
-    display(head);
+    struct Node* temp = stack->top;
+    int poppedData = temp->data;
+    stack->top = stack->top->next;
+    if (stack->top != NULL) {
+        stack->top->prev = NULL;
+    }
+    free(temp);
 
-
+    printf("%d popped from the stack\n", poppedData);
+    return poppedData;
 }
+
+int peek(struct Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Stack is empty! Cannot peek.\n");
+        return -1;
+    }
+    return stack->top->data;
+}
+
+
+void display(struct Stack* stack) {
+    if (isEmpty(stack)) {
+        printf("Stack is empty.\n");
+        return;
+    }
+
+    struct Node* temp = stack->top;
+    printf("Stack (top to bottom): ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+
+int main() {
+    struct Stack stack;
+    initStack(&stack);
+
+    push(&stack, 10);
+    push(&stack, 20);
+    push(&stack, 30);
+
+    display(&stack);
+
+    printf("Top element is %d\n", peek(&stack));
+
+    pop(&stack);
+    display(&stack);
+
+    pop(&stack);
+    pop(&stack);
+    pop(&stack);  
+
+    return 0;
+}
+
